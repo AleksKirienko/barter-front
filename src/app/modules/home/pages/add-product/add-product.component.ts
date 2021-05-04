b7030787e6c3d6626f95cdebae16a73fda5c6a50
AleksKirienko <aleks.Kirienko@bk.ru>
2021-05-04 18:33:11 +0700

    Добавила валидацию для формы добавления товара

--- src/app/modules/home/pages/add-product/add-product.component.ts
+++ src/app/modules/home/pages/add-product/add-product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Status } from '../../../../core/models/status';
+import { Router } from '@angular/router';
+import { ErrorMessages } from '../../../auth/pages/registration/error-messages';
 
 @Component({
   selector: 'app-add-product',
@@ -12,11 +14,30 @@ export class AddProductComponent implements OnInit {
   public newProductForm: FormGroup;
   public status: Status;
   public statuses: string[] = ['application', 'x-shader', 'video', 'image'];
+  formErrors = ErrorMessages;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router) {
+  }
 
   ngOnInit(): void {
-    console.log('statuses: ', this.statuses);
+    this.newProductForm = this.formBuilder.group({
+      nameProduct: ['', Validators.required],
+      status: ['', Validators.required],
+      descriptionProduct: ['', Validators.required],
+      exchangeOffer: ['', Validators.required],
+      exchanger: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      imgUrl: ['', [Validators.required]],
+    });
+
+  }
+
+  public onSubmit(): void {
+    if (this.newProductForm.valid) {
+      alert('Add new product success!');
+    }
   }
 
 }
